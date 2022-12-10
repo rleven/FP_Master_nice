575fb661803ec230c792136b021826abbd7f063b
maaxxns <max.koch@udo.edu>
2022-12-10 11:40:42 +0100

    Made plot for integrator

--- V51_Operationsverstärker/plots.py
+++ V51_Operationsverstärker/plots.py
@@ -35,9 +35,9 @@ print('Average Values of amplification: V1 ', V1, ' V2 ', V2, ' V3 ', V3)
 print('calculated amplifications: V1', -100/1, ' V2 ', -100/47, ' V3 ', -220/47)
 
 #fit in the decreasing area
-params1, cov1 = curve_fit(fit,np.log(data1[4:, 0]), np.log(ampli(U_in_1, data1[4:,2])), p0=(1,1))
-params2, cov2 = curve_fit(fit,np.log(data2[8:, 0]), np.log(ampli(U_in_2, data2[8:,2])), p0=(1,1))
-params3, cov3 = curve_fit(fit,np.log(data3[7:, 0]), np.log(ampli(U_in_3, data3[7:,2])), p0=(1,1))
+params1, cov1 = curve_fit(fit,(data1[4:, 0]), (ampli(U_in_1, data1[4:,2])), p0=(1,1))
+params2, cov2 = curve_fit(fit,(data2[8:, 0]), (ampli(U_in_2, data2[8:,2])), p0=(1,1))
+params3, cov3 = curve_fit(fit,(data3[7:, 0]), (ampli(U_in_3, data3[7:,2])), p0=(1,1))
 
 x1 = np.linspace(8000, 200000, 10000)
 x2 = np.linspace(4*10**5, 1.5*10**6, 10000)
@@ -54,12 +54,55 @@ x = ([x1, x2, x3])
 for i in range(3):
     plt.figure()
     plt.plot(data[i][:,0], ampli(U_in_1, data[i][:,2]),'rx', label='Gemessene Verstärkung')
-    plt.plot(x[i] , fit(x[i], params[i][0], params[i][1]), label='Fit')
+    plt.plot(x[i] , (fit(x[i], (params[i][0]), (params[i][1]))), label='Fit')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('Frequenz in Hz')
     plt.ylabel('Verstärkung')
     plt.legend()
     plt.savefig('build/inv_lin_' + str(i+1) + '.pdf')
+#    plt.show()
+plt.close()
+
+# Frequency depends of the phase
+
+for i in range(3):
+    plt.figure()
+    plt.plot(data[i][:,0], data[i][:,1],'rx', label='Gemessene Phase')
+#   plt.plot(x[i] , (fit(x[i], (params[i][0]), (params[i][1]))), label='Fit')
+    plt.xscale('log')
+#   plt.yscale('log')
+    plt.xlabel('Frequenz in Hz')
+    plt.ylabel('Phase / °')
+    plt.legend()
+    plt.savefig('build/inv_lin_' + str(i+1) + '.pdf')
+#    plt.show()
+plt.close()
 
 ################################################################################
+#
+#   Umkehr-Integrator
+#
+################################################################################
+
+# Suchen Sie einen sinnvollen Frequenzbereich und machen Sie eine Ausgleichsgrade. Ermitteln
+# Sie daraus die in den Vorbereitungen ermittelte Proportionalit ̈at. Zeigen Sie ihre Eingangssignale im
+# Vergleich zu den resultierten Integrierten Ausgangssignale
+
+
+data4 = np.genfromtxt('data/umkehr_int.csv', comments='#', delimiter=',') #u in, u out, freq
+
+params4, cov4 = curve_fit(fit,(data4[:4, 2]), data4[:4,1], p0=(1,1))
+print('fit4 parameters: a ', un.ufloat(params4[0], np.sqrt(cov4[0,0])), ' b ', un.ufloat(params4[1], np.sqrt(cov4[1,1])))
+x1 = np.linspace(2, 10**3, 10000)
+
+plt.figure()
+plt.plot(data4[:,2], data4[:,1],'rx', label='Gemessene Spannung')
+plt.plot(x1, fit(x1, params4[0], params4[1]), label='Fit')
+plt.xlabel('Frequenz in Hz')
+plt.ylabel('Ausgangsspannung / V')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend()
+plt.savefig('build/Umkehr_int.pdf')
+#plt.show()
