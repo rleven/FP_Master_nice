c98dfeec5e281431af478e1f0c72afa408ca7107
maaxxns <max.koch@tu-dortmund.de>
2022-12-28 15:14:11 +0100

    directory for V01 kosmische Myonen

---
+++ V01_kosmische_Myonen/plots.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from scipy.constants import mu_0 , h , elementary_charge , electron_mass, hbar , eV
